13e4a54276b94f466bfbaac5d98a14c43f659465
John Damask <johndamask@Johns-MacBook-Pro-2.local>
2025-02-02 11:41:19 -0500

    replacing code that was mistakenly deleted....sigh

---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export interface Tag {
+  id: string;
+  name: string;
+}
+
+export interface TagStore {
+  searchTags: (query: string) => Promise<Tag[]>;
+  createTag: (name: string) => Promise<Tag>;
+  getAllTags: () => Promise<Tag[]>;
+} 
